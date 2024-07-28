5d32901591ba26afab9b0610fa15faf55394a3d7
AdityaWjy <adamfadzal662@gmail.com>
2024-07-29 01:00:15 +0700

    fix ui footer

---
+++ src/components/footercomponent.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <!-- Footer -->
+    <footer
+      class="text-center text-lg-start text-white"
+      style="background-color: #262626"
+    >
+      <!-- Section: Links  -->
+      <section class="py-1">
+        <div class="container text-center text-md-start mt-5">
+          <!-- Grid row -->
+          <div class="row mt-3">
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-4 col-xl-4 mx-auto">
+              <!-- Content -->
+
+              <img src="../assets/logo.png" alt="Gafin Labs" class="w-50" />
+
+              <p>
+                Di Waifu Wonderland, kami merayakan kecintaan Anda terhadap
+                waifu! <br />
+                cuman ada layout mobile hehe malas bikin desktop
+              </p>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div
+              class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0"
+              style="margin-top: 40px"
+            >
+              <!-- Links -->
+              <h6 class="text-uppercase fw-semibold mb-4">Contact</h6>
+
+              <a
+                href="https://maps.app.goo.gl/gH8XMLGagBtBGQ3A9"
+                target="_blank"
+                class="block text-decoration-none text-white d-xl-flex align-items-center justify-items-center gap-2"
+              >
+                <img
+                  src="../assets/social/location-marker.png"
+                  alt="Location"
+                  class="mb-2"
+                  width="25"
+                />
+                <p
+                  class="d-flex align-items-center justify-items-center text-center text-xl-start pt-2 pt-xl-0"
+                >
+                  Perusahaan Waifu Wonderland berada di Kecamatan Bangsal,
+                  Kabupaten Jomokerto.
+                </p>
+              </a>
+
+              <a
+                href="https://www.instagram.com/ipologize/"
+                target="_blank"
+                class="block text-decoration-none text-white d-xl-flex align-items-center justify-align-center gap-2"
+              >
+                <img
+                  src="../assets/social/instagram.png"
+                  alt="Instagram"
+                  class="mb-2"
+                  width="25"
+                />
+                <p class="text-center text-xl-start pt-1">@ipologize</p>
+              </a>
+            </div>
+            <!-- Grid column -->
+          </div>
+          <!-- Grid row -->
+        </div>
+      </section>
+      <!-- Section: Links  -->
+
+      <!-- Copyright -->
+      <div
+        class="text-center p-4"
+        style="background-color: rgba(0, 0, 0, 0.05)"
+      >
+        © 2024 Copyright: <br />
+        <a
+          class="text-reset fw-bold"
+          href="https://www.instagram.com/ipologize/"
+          target="_blank"
+          >aditya aw aw</a
+        >
+      </div>
+      <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
+  </div>
+</template>
--- src/components/waifu.vue
+++ src/components/waifu.vue
@@ -156,122 +156,41 @@
 
     <!-- card waifu start-->
 
-    <div
-      class="container row-card d-flex jutify-content-center align-items-center gap-3 mt-5 mb-5"
-      id="list"
-    >
-      <div
-        class="card p-2 border-dark d-flex justify-content-center align-items-center"
-        v-for="waifu in waifus"
-        :key="waifu.id"
-      >
-        <img
-          :src="waifu.imageUrl"
-          class="card-img-top image-service rounded"
-          alt="Waifu"
-        />
-        <div class="mt-3">
-          <h5 class="card-title text-center mb-3">{{ waifu.name }}</h5>
-
-          <button class="btn btn-dark" style="width: 120px">Delete</button>
+    <div class="container mt-4">
+      <div class="row row-cols-md-2 row-cols-lg-3 g-4">
+        <div class="col" v-for="waifu in waifus" :key="waifu.id">
+          <div
+            class="card h-100 p-2 border-dark d-flex justify-content-center align-items-center"
+          >
+            <img
+              :src="waifu.imageUrl"
+              class="card-img-top rounded"
+              alt="Waifu"
+            />
+            <div class="mt-3">
+              <h5 class="card-title text-center mb-3">{{ waifu.name }}</h5>
+              <button
+                @click="deleteWaifu(waifu.id)"
+                class="btn btn-dark"
+                style="width: 120px"
+              >
+                Delete
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
 
-    <!-- card waitu end -->
+    <!-- card waifu end -->
 
-    <!-- Footer -->
-    <footer
-      class="text-center text-lg-start text-white"
-      style="background-color: #262626"
-    >
-      <!-- Section: Links  -->
-      <section class="py-1">
-        <div class="container text-center text-md-start mt-5">
-          <!-- Grid row -->
-          <div class="row mt-3">
-            <!-- Grid column -->
-            <div class="col-md-3 col-lg-4 col-xl-4 mx-auto">
-              <!-- Content -->
-
-              <img src="../assets/logo.png" alt="Gafin Labs" class="w-50" />
-
-              <p>
-                Di Waifu Wonderland, kami merayakan kecintaan Anda terhadap
-                waifu! <br />
-                cuman ada layout mobile hehe malas bikin desktop
-              </p>
-            </div>
-            <!-- Grid column -->
-
-            <!-- Grid column -->
-            <div
-              class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0"
-              style="margin-top: 40px"
-            >
-              <!-- Links -->
-              <h6 class="text-uppercase fw-semibold mb-4">Contact</h6>
-
-              <a
-                href="https://maps.app.goo.gl/gH8XMLGagBtBGQ3A9"
-                target="_blank"
-                class="block text-decoration-none text-white d-xl-flex align-items-center justify-items-center gap-2"
-              >
-                <img
-                  src="../assets/social/location-marker.png"
-                  alt="Location"
-                  class="mb-2"
-                  width="25"
-                />
-                <p
-                  class="d-flex align-items-center justify-items-center text-center text-xl-start pt-2 pt-xl-0"
-                >
-                  Perusahaan Waifu Wonderland berada di Kecamatan Bangsal,
-                  Kabupaten Jomokerto.
-                </p>
-              </a>
-
-              <a
-                href="https://www.instagram.com/ipologize/"
-                target="_blank"
-                class="block text-decoration-none text-white d-xl-flex align-items-center justify-align-center gap-2"
-              >
-                <img
-                  src="../assets/social/instagram.png"
-                  alt="Instagram"
-                  class="mb-2"
-                  width="25"
-                />
-                <p class="text-center text-xl-start pt-1">@ipologize</p>
-              </a>
-            </div>
-            <!-- Grid column -->
-          </div>
-          <!-- Grid row -->
-        </div>
-      </section>
-      <!-- Section: Links  -->
-
-      <!-- Copyright -->
-      <div
-        class="text-center p-4"
-        style="background-color: rgba(0, 0, 0, 0.05)"
-      >
-        © 2024 Copyright: <br />
-        <a
-          class="text-reset fw-bold"
-          href="https://www.instagram.com/ipologize/"
-          target="_blank"
-          >aditya aw aw</a
-        >
-      </div>
-      <!-- Copyright -->
-    </footer>
-    <!-- Footer -->
+    <footercomponent class="mt-5" />
   </div>
 </template>
 
 <script>
+import footercomponent from "../components/footercomponent.vue";
+// import firebase
 import { ref } from "vue";
 import { db, storage } from "../firebase";
 import {
@@ -288,11 +207,17 @@ import {
 } from "firebase/storage";
 
 export default {
+  components: {
+    footercomponent,
+  },
+
   setup() {
+    // set tipe data
     const waifus = ref([]);
     const waifuName = ref("");
     const file = ref(null);
 
+    // function get data waifuu
     const fetchWaifus = async () => {
       try {
         const querySnapshot = await getDocs(collection(db, "waifus"));
@@ -309,6 +234,7 @@ export default {
       file.value = event.target.files[0];
     };
 
+    // create data waifu
     const createWaifu = async () => {
       if (!file.value || !waifuName.value) return;
 
@@ -330,11 +256,21 @@ export default {
       }
     };
 
-    const deleteWaifu = () => {
-      alert("Hehe gabisa di delete ya bang?");
+    // delete data waifu
+
+    const deleteWaifu = async (id) => {
+      await deleteDoc(doc(db, "waifus", id));
+      fetchWaifus();
     };
 
+    // call function fecth data waifu
     fetchWaifus();
+
+    // clear input form
+    const clearInput = () => {
+      waifuName.value = "";
+      file.value = "";
+    };
 
     return {
       waifus,
@@ -371,13 +307,13 @@ export default {
   box-shadow: 0 0 5px #ff4191; /* Red Shadow */
 }
 
-@media (min-width: 992px) {
+@media (max-width: 500px) {
   .row-card {
     row-gap: 15px;
   }
 
   .card {
-    width: 10rem;
+    width: fit-content;
   }
 
   img {
